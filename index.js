bb7e0bec3e9cd210d8d65e8c1e81765d1d05e48f
rh-abir <rhabir.co@gmail.com>
2023-06-17 23:25:28 +0600

    added midleware

--- index.js
+++ index.js
@@ -10,5 +10,20 @@ import path from "path";
 
 import { fileURLToPath } from "url";
 
-
 /*Configurations*/
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+dotenv.config();
+const app = express();
+
+/*midlewar*/
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("assets", express.static(path.join(dirname, "public/assets")))
